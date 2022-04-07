9ad333f5fa3465f4a9cf17ec583815596d96de6b
Marios Pakas <pakmarios@gmail.com>
2022-04-07 22:37:15 +0300

    implement data_mem.v

---
+++ data_mem.v
@@ -0,0 +1,23 @@
+module data_mem (
+    input wire reset,
+    input wire clock,
+    input wire[31:0] result,
+    input wire mem_wr,
+    input wire mem_rd,
+    input wire[31:0] rdb,
+    output wire[31:0] read_data
+);
+
+    // this is not a proper implementation of ram, just a dummy register
+    reg[128] data_ram
+
+    always @(posedge clock) begin
+        if(!reset) begin
+            if (mem_wr)
+                data_ram[result] = rdb'
+            else if (mem_rd)
+                read_data = data_ram[result];
+        end
+    end
+
+endmodule
