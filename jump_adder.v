c254a234ba859abfa921ac779d976a81005616b8
Marios Pakas <pakmarios@gmail.com>
2022-04-07 23:17:22 +0300

    fixed a space

--- jump_adder.v
+++ jump_adder.v
@@ -1,6 +1,6 @@
 module jump_adder (
     input wire reset,
-    input wire[31:0]  previous_pc,
+    input wire[31:0] previous_pc,
     input wire[31:0] jump_adder
     output wire[31:0] jump_addr
 );
