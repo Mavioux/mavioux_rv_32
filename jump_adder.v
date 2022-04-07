020e16b44663b42a5237fadde6ec294fd77709a2
Marios Pakas <pakmarios@gmail.com>
2022-04-07 23:17:05 +0300

    rename read_addr to previous_pc

--- jump_adder.v
+++ jump_adder.v
@@ -1,13 +1,13 @@
 module jump_adder (
     input wire reset,
-    input wire[31:0] read_addr,
+    input wire[31:0]  previous_pc,
     input wire[31:0] jump_adder
     output wire[31:0] jump_addr
 );
 
     always @* begin
         if (!reset) begin
-            jump_addr = read_addr + jump_adder;
+            jump_addr = previous_pc + jump_adder;
         end
     end
 
